505a98043233b662638f0e5e2e92212ff5950c7b
anjali <anjalisharamaom2568@gmail.com>
2022-10-20 16:40:12 +0530

    consecutive number assignmnt

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,108 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
+// router.get('/students/:name', function(req, res) {
+//     let studentName = req.params.name
+//     console.log(studentName)
+//     res.send(studentName)
+// })
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
-
-
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
+// router.get("/random" , function(req, res) {
+//     res.send("hi there")
+// })
 
 
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
+// router.get("/test-api" , function(req, res) {
+//     res.send("hi FunctionUp")
+// })
 
 
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
+// router.get("/test-api-2" , function(req, res) {
+//     res.send("hi FunctionUp. This is another cool API")
+// })
 
 
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
+// router.get("/test-api-3" , function(req, res) {
+//     res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
+// })
 
 
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
+// router.get("/test-api-4" , function(req, res) {
+//     res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+// })
 
 
 
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
+// router.get("/test-api-5" , function(req, res) {
+//     res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+// })
+
+// router.get("/test-api-6" , function(req, res) {
+//     res.send({a:56, b: 45})
+// })
+
+// router.post("/test-post", function(req, res) {
+//     res.send([ 23, 45 , 6])
+// })
+
+
+// router.post("/test-post-2", function(req, res) {
+//     res.send(  { msg: "hi" , status: true }  )
+// })
+
+// router.post("/test-post-3", function(req, res) {
+//     // let id = req.body.user
+//     // let pwd= req.body.password
+
+//     // console.log( id , pwd)
+
+//     console.log( req.body )
+
+//     res.send(  { msg: "hi" , status: true }  )
+// })
+
+
+
+// router.post("/test-post-4", function(req, res) {
+//     let arr= [ 12, "functionup"]
+//     let ele= req.body.element
+//    arr.push(ele)
+ //    res.send(  { msg: arr , status: true }  )
+ //})
+  router.get("/sol1", function (req, res) {
+     let array=[1,2,3,5,6,7]
+      let n=array.length+1 
+     let sumOfnaturalNumber =n*(n+1)/2
+     let sumOfarray=0
+     for(let i=0;i<array.length;i++){
+         let element = array[i]
+         sumOfarray+=element
+      }
+      let result = sumOfnaturalNumber-sumOfarray
+     res.send( {  missingNumber: result } );
+    });
+
+ router.get("/sol2", function (req, res) {
+
+    let arr= [33, 34, 35, 37, 38]
+    let n=arr.length+1 
+     let sumOfnaturalNumber =n*(33+38)/2  //logic : sum of n consecutive numbers is [ n * (first + last) / 2 ]..so get sum of all
+   let sumOfarray=0
+    for(let i=0;i<arr.length;i++){
+        let element = arr[i]
+        sumOfarray+=element
+    }
+    let result = sumOfnaturalNumber-sumOfarray
+    res.send( { missingNumber: result } );
+    
+     } );
+
+
+ 
+
+
+
+
+
 
 module.exports = router;
